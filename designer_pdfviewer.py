c591ff07269e667e732bd8c442d26cf1ba19177a
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-02-01 09:17:36 +0300

    solve

--- designer_pdfviewer.py
+++ designer_pdfviewer.py
@@ -1,5 +1,9 @@
 def designerPdfViewer(h, word):
+    difference = 0
     
+    for i in word:
+        difference = max(difference, h[ord(i) - ord('a')]) # ord = ASCII number
     
+    return difference*len(word)
     
 print(designerPdfViewer([1,3,1,3,1,4,1,3,2,5,5,5,5,1,1,5,5,1,5,2,5,5,5,5,5,5], "torn"))

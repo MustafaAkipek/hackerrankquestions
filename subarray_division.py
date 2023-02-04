5369e4822a9cf6eac2b37062d4c1194a5a9a2743
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-02-04 09:37:13 +0300

    update

--- subarray_division.py
+++ subarray_division.py
@@ -1,14 +1,18 @@
+# int s[n] = the numbers on each of the squares of chocolate
+# int d: Ron's birth day
+# int m: Ron2s birth month
+
 def subarray_division(s, d, m):
     i = 0
     j = m
-    count = 0
+    dw = 0
     
     while j <= len(s):
         if sum(s[i:j]) == d:
-            count += 1
+            dw += 1
         i += 1
         j += 1
     
-    return count
-
+    return dw
+ 
 print(subarray_division([1, 2, 1, 3, 2], 3, 2))

b4a20f40d25f81e3a80b5a6a9d77c826877f2424
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-02-13 09:33:30 +0300

    solution

---
+++ numberlinejumps.py
@@ -0,0 +1,14 @@
+# int x1, int v1: starting position and jump distance for kangaroo 2
+# int x2, int v2: starting position and jump distace for kangaroo 2
+
+
+def numberlinejumps(x1, v1, x2, v2):
+    for i in range(10000):
+        x1 += v1
+        x2 += v2
+        if x1 == x2:
+            return "YES"
+    else:
+        return "NO"
+
+print(numberlinejumps(0, 3, 4, 2))

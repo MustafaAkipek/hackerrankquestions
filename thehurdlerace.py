0463e58409e9cb3a7697e2804ecdeae0a00c3425
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-02-24 09:13:26 +0300

    solution

---
+++ thehurdlerace.py
@@ -0,0 +1,11 @@
+# int k: the height the character can jump naturally
+# int height[n]: the heights of each hurdle
+
+def hurdleRace(k, height):
+    peak = max(height)
+    if k > peak:
+        return 0
+    else:
+        return peak - k
+
+print(hurdleRace(1, [1,2,3,3,2]))

ffa3840ee8925bf6c761cefa98dff52d2a4fefeb
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-02-25 09:00:53 +0300

    update

--- plusminus.py
+++ plusminus.py
@@ -1,19 +1,21 @@
-def plusMinus(a):
+# int arr[n]: an array of integers
+
+def plusMinus(arr):
     presult = 0
     nresult = 0
     zeron = 0
     
     for i in range(0,5):
-        if a[i] > 0:
+        if arr[i] > 0:
             presult += 1
-        if a[i] == 0:
+        if arr[i] == 0:
             zeron += 1
-        if a[i] < 0:
+        if arr[i] < 0:
             nresult += 1
             
-    positiveration = presult/len(a)
-    negativeration = nresult/len(a)
-    zeroration = zeron/len(a) 
+    positiveration = presult/len(arr)
+    negativeration = nresult/len(arr)
+    zeroration = zeron/len(arr) 
        
     return f"{positiveration:.6f}\n{negativeration:.6f}\n{zeroration:.6f}"
     

b6f2e5e9f01bd729a923057a2958867414e13c3b
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-02-25 09:20:56 +0300

    solution

--- utopiantree.py
+++ utopiantree.py
@@ -1,3 +1,15 @@
-def utopianTree(n):
-    pass
+# int n: the number of growth cycles to simulate
 
+def utopianTree(n):
+    stage = 0
+    height = 1
+    while (stage < n):
+        stage += 1
+        if stage % 2 == 0:
+            height += 1
+        else:
+            height *= 2
+    
+    return height
+
+print(utopianTree(4))

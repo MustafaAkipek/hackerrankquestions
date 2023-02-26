153b73b94adc4839ed9c60616f5733eab35db24a
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-02-26 16:23:05 +0300

    update

--- staircase.py
+++ staircase.py
@@ -1,9 +1,11 @@
-n = 10 
+# int n: an integer
 
-i = 1 
-j = n-1 
-while(i <= n):
-    print(" "*(j)+i*"#")
-    i += 1
-    j -= 1
-    
+def staircase(n):
+    i = 1 
+    j = n-1 
+    while(i <= n):
+        print(" "*(j)+i*"#")
+        i += 1
+        j -= 1
+
+staircase(4)

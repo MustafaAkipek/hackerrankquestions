a750eb89923db12a6f7a7347c232157cbc41161e
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-03-02 08:58:27 +0300

    update

--- betweentwosets.py
+++ betweentwosets.py
@@ -2,27 +2,28 @@
 # int b[m]: an array of integers
 
 def getTotal(a, b):
-   factors_a = []
-   factors_b = []
-   common_factors = []
-   max_len = len(a + b)
-   
-   for i in range(1, 101):
-      for number in a:
-         if i % number == 0:
-            factors_a.append(i) 
-   
-   for i in range(1,101):
-      for number in b:
-         if number % i == 0:
-            factors_b.append(i)
-            
-   union_list = factors_a + factors_b
-   
-   for number in union_list:
-      if union_list.count(number) == max_len:
-         common_factors.append(number)
+   alist = []
+   blist = []
+   unionl = []
+   maxlen = len(a+b)
+   common = []
+   for i in range(len(a)):
+      start = 0
+      while start < min(b):
+         start += a[i]
+         alist.append(start)
          
-   return len(set(common_factors))
+   for i in range(len(b)):
+      for j in range(b[i],0,-1):
+         if b[i] % j == 0:
+            blist.append(j)  
+            
+   unionl = alist + blist
+    
+   for number in unionl:
+      if unionl.count(number) == maxlen:
+         common.append(number)
+         
+   return len(set(common))        
    
 print(getTotal([2,6],[24,36]))

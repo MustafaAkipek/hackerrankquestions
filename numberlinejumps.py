b67fad31c0bc68fabc30f7e6d9e171fd2e2f1911
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-03-03 12:05:51 +0300

    update

--- numberlinejumps.py
+++ numberlinejumps.py
@@ -1,7 +1,6 @@
 # int x1, int v1: starting position and jump distance for kangaroo 2
 # int x2, int v2: starting position and jump distace for kangaroo 2
 
-
 def numberlinejumps(x1, v1, x2, v2):
     for i in range(10000):
         x1 += v1

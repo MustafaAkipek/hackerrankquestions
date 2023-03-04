94022af8a6314b976857f31d622b09b28a9920ad
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-03-04 15:39:56 +0300

    update

--- migratory_birds.py
+++ migratory_birds.py
@@ -1,9 +1,9 @@
 def migratoryBirds(arr):
     nlist = [0] * len(arr)
-   
+    
     for i in range(len(arr)):
         nlist[arr[i]] += 1
-    
+        
     return nlist.index(max(nlist))
     
-print(migratoryBirds(arr=[1, 1, 2, 2, 3]))
+print(migratoryBirds([1, 4, 4, 4, 5, 3]))

053ed110a5dd7508b170bc6946d1581ecc5e1f48
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-03-06 12:21:44 +0300

    solution

---
+++ finddigits.py
@@ -0,0 +1,16 @@
+# int n: the value of to analyze
+
+def findDigits(n):
+    timer = 0
+    l = (len(str(n)))
+    
+    for i in range(int(l)):
+        
+        n = str(n)
+        if int(n[i]) != 0:
+            if int(n) % int(n[i]) == 0:
+                timer += 1
+            
+    return timer
+    
+print(findDigits(124))

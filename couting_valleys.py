8e597f903725a6c1f01e09d3a7e9b6c5c5ca5bb9
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-03-07 15:12:39 +0300

    update

--- couting_valleys.py
+++ couting_valleys.py
@@ -1,20 +1,19 @@
-def countingValleys(steps, path):
+# int steps: the number of steps on the hike
+# string path: a string describing the path
 
+def countingValleys(steps, path):
     level = 0  
     valleys = 0  
     
     for step in path:
-    
         if step == 'U':
             level+=1
-            
             if level == 0:
                 valleys+=1
                 
         else:
             level-=1
     
-            
     return (valleys)
 
 print(countingValleys(8, "UDDDUDUU"))

927c4b614d14bef3c4a1ccc0bb4a86a81eb4b4ee
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-03-07 08:50:48 +0300

    wait

---
+++ appendanddelete.py
@@ -0,0 +1,8 @@
+# string s: the intial string
+# string t: the desired string
+# int k: the exact number of operaitons that must be performed
+
+def appendAndDelete(s, t, k):
+    pass
+    
+print(appendAndDelete("hackerhappy","hackerrank"))

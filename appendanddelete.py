ecd59fc5aaab6d69356a9e6d253f440f125d77c3
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-03-08 09:01:28 +0300

    solution

--- appendanddelete.py
+++ appendanddelete.py
@@ -3,6 +3,35 @@
 # int k: the exact number of operaitons that must be performed
 
 def appendAndDelete(s, t, k):
-    pass
+    cs = 0
+    ct = 0
     
-print(appendAndDelete("hackerhappy","hackerrank"))
+    if len(s) < len(t):
+        for i in range(len(s)):
+            if s[i] == t[i]:
+                cs += 1
+                ct += 1
+            else:
+                break
+    
+    else:
+        for i in range(len(t)):
+            if s[i] == t[i]:
+                cs += 1
+                ct += 1
+            else:
+                break
+                
+    mneed = (len(s) - cs) + (len(t) - ct)
+    
+
+    if k > len(s) + len(t):
+        return "Yes"
+    
+    if k >= mneed and  (k - mneed) % 2 == 0 :
+        return "Yes"
+    
+    else:
+        return "No"
+    
+print(appendAndDelete("hackerhappy", "hackerrank", 9))

6fe89f852161151cc8646ffdd18334ffafd50932
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-03-14 19:44:52 +0300

    solution

--- cutthesticks.py
+++ cutthesticks.py
@@ -1,6 +1,15 @@
 # int arr[n]: the lengths of each stick
+from collections import Counter
 
 def cutTheSticks(arr):
-    pass
-                
-cutTheSticks([5, 4, 4, 2, 2, 8])
+    result = []
+    n = len(arr)
+    s = Counter(arr)
+    
+    for i in sorted(s.keys()):
+        result.append(n)
+        n -= s[i]
+        
+    return result
+        
+print(cutTheSticks([5, 4, 4, 2, 2, 8]))

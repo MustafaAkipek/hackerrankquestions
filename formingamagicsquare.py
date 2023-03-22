4151454abb40bd43c9e3039e4ac13f3861108552
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-03-22 22:31:52 +0300

    wait

---
+++ formingamagicsquare.py
@@ -0,0 +1,8 @@
+# int s[3][3]: a 3x3 array of integers
+
+def formingMagicSquare(s):
+    pass
+
+print(formingMagicSquare([5,3,4],
+                         [1,5,8],
+                         [6,4,2]))

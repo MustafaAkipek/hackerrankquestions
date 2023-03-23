0866b97ceed0738e430df1008e1969f5899456ee
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-03-23 22:44:29 +0300

    wait

--- formingamagicsquare.py
+++ formingamagicsquare.py
@@ -3,6 +3,6 @@
 def formingMagicSquare(s):
     pass
 
-print(formingMagicSquare([5,3,4],
-                         [1,5,8],
-                         [6,4,2]))
+print(formingMagicSquare([[5,3,4],
+                          [1,5,8],
+                          [6,4,2]]))

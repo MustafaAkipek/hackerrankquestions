af9ea35b3de959197439017186d584f2db920c6e
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-04-01 14:59:18 +0300

    solution

---
+++ acm-ıcpcteam.py
@@ -0,0 +1,24 @@
+# string topic: a string of binary digits
+
+
+def acmTeam(topic):
+    maxk = []
+    gek = list(topic)
+    for i in range(len(topic[0])-1):
+        for j in range(i+1,len(gek)):
+            t = 0
+            for k in range(len(topic[i])):
+                if topic[i][k] == "1":
+                    t += 1
+                else:
+                    if topic[j][k] == "1":
+                        t += 1
+                        
+            maxk.append(t)
+    
+    maxkc = max(maxk)
+    maxkt = maxk.count(maxkc)
+
+    return maxkc, maxkt
+
+print(acmTeam(["10101","11100","11010","00101"]))

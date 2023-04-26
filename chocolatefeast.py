1db1062f09d62647679c70c4d1514110dad26ba2
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-04-26 09:59:12 +0300

    wait

---
+++ chocolatefeast.py
@@ -0,0 +1,8 @@
+# int n: Bobby's initial amount of money
+# int c: the cost of a chocolate bar
+# int m: the number of wrappers he can turn in for a free bar
+
+def chocolateFeast(n, c, m):
+    pass
+
+print(chocolateFeast(15, 3, 2))

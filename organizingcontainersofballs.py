b7932169398bbd5cc30c56e8836ef8dbde3d22dc
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-05-10 19:44:21 +0300

    wait

---
+++ organizingcontainersofballs.py
@@ -0,0 +1,6 @@
+# int container[n][m]: a two dimensional array of integers that represent the number of balls of each color in each container
+
+def organizingContainers(container):
+    pass
+
+print(organizingContainers([[1,4],[1,3]]))

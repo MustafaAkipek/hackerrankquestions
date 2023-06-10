8c29b1058e32ab1483bee7ff8f7709213b14f1f4
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-06-10 20:14:04 +0300

    solution

--- manasaandstones.py
+++ manasaandstones.py
@@ -3,7 +3,18 @@
 # int b: another possible integer difference
 
 def stones(n, a, b):
-    res = []
+    result = set()
+
+    for i in range(n):
+        result.add(i*a + (n-1-i)*b)
+
+    return sorted(list(result))
+
+print(stones(3, 1, 2))
+
+# my solution but dont support 4 case(because of the repetitive elements)
+"""
+res = []
     for k in range(n):
         for m in range(n-k):
             if k + m == n-1:
@@ -13,5 +24,4 @@ def stones(n, a, b):
     res.sort()
 
     return res
-
-print(stones(3, 1, 2))
+"""
